e797101bc554da6728d91365a0218936b73f0a0a
Pasindu Yapa <139765303+pasinduy@users.noreply.github.com>
2024-01-19 01:20:54 +0530

    first commit

---
+++ src/main/java/lk/ijse/Controller/LoginFormController.java
@@ -0,0 +1,16 @@
+package lk.ijse.Controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+
+public class LoginFormController {
+    public JFXTextField txtPassword;
+    public ComboBox usernameCmbBox;
+
+    public void btnSignInOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
+}
