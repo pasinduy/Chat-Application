06b81748904692590dd3f804320ccdffb96068d0
Pasindu Yapa <139765303+pasinduy@users.noreply.github.com>
2024-01-22 00:38:14 +0530

    Finished the FXML files

--- src/main/java/lk/ijse/Controller/SignInFormController.java
+++ src/main/java/lk/ijse/Controller/SignInFormController.java
@@ -1,4 +1,16 @@
 package lk.ijse.Controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 public class SignInFormController {
+    public JFXTextField txtName;
+    public JFXTextField txtUsername;
+    public JFXTextField txtPassword;
+
+    public void btnSignUpOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnClearOnAction(ActionEvent actionEvent) {
+    }
 }
